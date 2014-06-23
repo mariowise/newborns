5d70951c21aa0c3a3375c3447cbcb6dce80f7d08
Mosheman <moshehefra.landaeta@usach.cl>
2014-06-22 20:14:03 -0400

    Poblamiento Profiles-Mothers-Deliveries-Sons

---
+++ db/populate-profile.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO profile (id, createdat)
+VALUES (124, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (125, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (126, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (127, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (128, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (129, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (130, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (131, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (132, '2014-06-09');
+INSERT INTO profile (id, createdat)
+VALUES (133, '2014-06-09');
